3af72a18864736d82cee4f831886f9399304f013
EvanGu <detached_gu@163.com>
2019-01-03 17:07:11 +0800

    Update Director

--- Auth/src/main/java/com/auth/controller/LoginController.java
+++ Auth/src/main/java/com/auth/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.auth;
+package com.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- Auth/src/main/java/com/auth/controller/UserController.java
+++ Auth/src/main/java/com/auth/controller/UserController.java
@@ -1,11 +1,15 @@
-package com.auth;
+package com.auth.controller;
 
-import org.springframework.stereotype.Controller;
+import com.auth.entity.OAuthAccessToken;
+import com.auth.mapper.OAuthAccessTokenMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.List;
 
 /**
  * @author: Gu danpeng
@@ -17,10 +21,19 @@ import java.security.Principal;
 @RestController
 public class UserController {
 
+    @Autowired
+    OAuthAccessTokenMapper oauthAccessTokenMapper;
+
     @ResponseBody
     @RequestMapping("me")
     public Principal getPhotoServiceUser(Principal principal) {
         System.out.println(principal);
         return principal;
+    }
+
+    @GetMapping("/token")
+    @ResponseBody
+    public List<OAuthAccessToken> getTest(){
+        return oauthAccessTokenMapper.selectAll();
     }
 }
