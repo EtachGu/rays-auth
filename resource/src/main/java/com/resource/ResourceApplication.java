8d3a1fa9ba5358c914d233f38e27da27a319e1cd
EvanGu <detached_gu@163.com>
2019-01-05 15:34:46 +0800

    add RemoteTokenService to Resource

--- resource/src/main/java/com/resource/ResourceApplication.java
+++ resource/src/main/java/com/resource/ResourceApplication.java
@@ -1,23 +1,46 @@
 package com.resource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @SpringBootApplication
 @EnableResourceServer
-public class ResourceApplication extends ResourceServerConfigurerAdapter {
+public class ResourceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceApplication.class, args);
 	}
 
-	@Override
-	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/test").access("#oauth2.hasScope('read')").anyRequest().permitAll();
+//	@Override
+//	public void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//				.antMatchers("/test").access("#oauth2.hasScope('read')").anyRequest().permitAll();
+//	}
+
+	@Value("${security.oauth2.resource.token-info-uri}")
+	String checkTokenUrl;
+
+	@Value("${security.oauth2.client.clientId}")
+	String clientId;
+
+	@Value("${security.oauth2.client.clientSecret}")
+    String clientSecret;
+
+	@Primary
+	@Bean
+	public RemoteTokenServices tokenService() {
+		RemoteTokenServices tokenService = new RemoteTokenServices();
+		tokenService.setCheckTokenEndpointUrl(checkTokenUrl);
+		tokenService.setClientId(clientId);
+		tokenService.setClientSecret(clientSecret);
+		return tokenService;
 	}
 }
 
