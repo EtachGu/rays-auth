b777486d2aae02e5f7daf17184f726c368483bc5
EvanGu <detached_gu@163.com>
2019-01-06 21:45:31 +0800

    add ResourceServer to Auth, protect '/rest'

---
+++ Auth/src/main/java/com/auth/config/ResourceServerConfiguration.java
@@ -0,0 +1,21 @@
+package com.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * @author: Gu danpeng
+ * @date: 2019-1-5
+ * @version：1.0
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/rest/**").authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
