6349129045e124ec5198545e676f2dc2d00f887d
EvanGu <detached_gu@163.com>
2019-03-05 21:21:44 +0800

    add create ClientDetails

--- Auth/src/main/java/com/auth/controller/api/ClientManageController.java
+++ Auth/src/main/java/com/auth/controller/api/ClientManageController.java
@@ -1,11 +1,11 @@
 package com.auth.controller.api;
 
+import com.auth.entity.OAuthClientDetails;
+import com.auth.mapper.OAuthClientDetailsMapper;
 import com.auth.service.ClientManageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.provider.ClientDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,9 +21,18 @@ public class ClientManageController {
     @Autowired
     private ClientManageService clientManageService;
 
+    @Autowired
+    private OAuthClientDetailsMapper oAuthClientDetailsMapper;
+
     @GetMapping("/all")
     public List<ClientDetails> getClientDetails(){
         return clientManageService.getClientDetails();
+    }
+
+    @PostMapping("")
+    public void createClientDetails(@RequestBody OAuthClientDetails oAuthClientDetails){
+        oAuthClientDetailsMapper.insert(oAuthClientDetails);
+//        clientManageService.addClientDetails(oAuthClientDetails);
     }
 
 }
