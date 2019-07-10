f0e22c2ffe0344719c340ef1ca6e9734f610618d
EvanGu <detached_gu@163.com>
2019-07-10 15:18:39 +0800

    update inti.sql

--- Auth/src/main/resources/database/init.sql
+++ Auth/src/main/resources/database/init.sql
@@ -7,4 +7,4 @@ INSERT INTO `evan_sso`.`roles_permissions` (`role_id`, `permission_id`) VALUES (
 
 INSERT INTO `evan_sso`.`users_roles` (`user_name`, `role_id`) VALUES ('superadmin', '1');
 
-
+INSERT INTO `evan_sso`.`oauth_user`(`user_name`,`password`) VALUES ('superadmin', '$2a$10$TFIavH5kFrxa5XwSCxJlLOy8xyOMXwvQR3fSO.o8vWOQNM1PkHcNa');
